ae734d2725a4a1d0008c2d4a3b384eec34d7eeaa
Gary Lloyd <gary.lloyd@advancedcomputersoftware.com>
2016-11-22 09:30:32 +0000

    Corrected for 2nd test, added next test.

--- DivisorsKata/DivisorCalculator.cs
+++ DivisorsKata/DivisorCalculator.cs
@@ -8,13 +8,7 @@ namespace DivisorsKata
     {
         public int[] Calculate(int inputNumber)
         {
-            //this is the one liner code that I could think of to pass the test
-            return inputNumber == 1 ? new int[] {1} : null;
-
-            //This is an alternative way but a more generic one
-            //List<int> list = new List<int>(new int[] {1});
-            //var divisors = list.Where(x => inputNumber % x == 0).ToArray();
-            //return divisors;
+            return inputNumber == 2 ? new int[] { 1, 2 } : new int[] {1};
         }
     }
 }
