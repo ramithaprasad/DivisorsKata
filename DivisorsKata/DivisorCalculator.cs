c440433a4298c5277cd9040d57314f77bd49f388
Ramitha Prasad <ramitha.prasad@oneadvanced.com>
2016-11-23 12:31:49 +0530

    Revert "Tdd"

--- DivisorsKata/DivisorCalculator.cs
+++ DivisorsKata/DivisorCalculator.cs
@@ -1,25 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
-namespace DivisorsKata
+﻿namespace DivisorsKata
 {
     public class DivisorCalculator
     {
         public int[] Calculate(int inputNumber)
         {
-            if (inputNumber == 4)
-            {
-                return new int[] {1,2,4};
-            }
-            else if (inputNumber == 2)
-            {
-                return new int[] { 1, 2};
-            }
-            else
-            {
-                return new int[] {1};
-            }
+            return null;
         }
     }
 }
